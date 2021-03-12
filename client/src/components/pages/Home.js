d33c643cf8fbf986dc49c83fd0ea09da1bb3bd4f
Zachary <rick.jsventures.com>
2021-03-11 23:44:47 -0500

    PlaceHolder home screen

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,21 +1,112 @@
 import React from 'react'; 
 import Particles from "react-tsparticles";
-import './App.css';
+import './Header.css';
 // import UnopDropdown from "unop-react-dropdown";
 
 
 function Home() { 
   return (
-    <div className="jumbotron">
-      <h1 className="welcome">Our Website</h1>
-      {/* <UnopDropdown trigger={<button>Click</button>}>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-        </ul>
-      </UnopDropdown> */}
+    <div>
+    
+      <div class="center">
+      
+        <h1>listen in</h1>
+      </div>
+      <div class="center">
+        stay tuned.
+      </div>
+      {<Background/>}
     </div>
   );
 }
+
+function Background() {
+  return(
+    <Particles
+        id="tsparticles"
+        
+        options={{
+          background: {
+            color: {
+              value: "#6BB3F2",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+       
+      />
+  )
+}
+
 
 export default Home; 
