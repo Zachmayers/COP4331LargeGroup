716bd11391c61ce80a8ec2e5b7fc2e8dcfea1c71
Zachary <rick.jsventures.com>
2021-03-11 22:24:46 -0500

    Attempting to create from scratch - Initial commit

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'; 
 import Particles from "react-tsparticles";
 import './App.css';
+// import UnopDropdown from "unop-react-dropdown";
 
 
 function Home() { 
   return (
     <div className="jumbotron">
       <h1 className="welcome">Our Website</h1>
+      {/* <UnopDropdown trigger={<button>Click</button>}>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+      </UnopDropdown> */}
     </div>
   );
 }
