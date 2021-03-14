68dbbcfaaaccd8ddd041095f6b07a2a341fba8fb
Zachary <rick.jsventures.com>
2021-03-13 19:10:30 -0500

    changed callback URI

--- client/src/components/pages/Login/config.js
+++ client/src/components/pages/Login/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "717d9cf9ead04d2fb7497fda4d407dfe";
-export const redirectUri = 'http://localhost:8888/Callback';
+export const redirectUri = 'listenin.com://callback';
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
