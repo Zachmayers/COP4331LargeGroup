9786a775328109fe587d0aa741a3cef865aa5e22
Camila <camila.larosap@gmail.com>
2021-03-18 23:02:12 -0400

    testing a few things

--- client/src/components/pages/Banner.js
+++ client/src/components/pages/Banner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Welcome from './Welcome.js'
+import Title from './Title.js';
 import './Banner.css'
 import fondo from './fondo.jpg';
 
 function Banner() {
     return (
         <div className="banner" >
+            <Title />
             <Welcome />
         </div>
     );
