5d1f4106a8ee67b58dd66dfa11fa2abbc78bc06e
Camila <camila.larosap@gmail.com>
2021-04-15 20:37:23 -0400

    quick fix

--- client/src/components/pages/Login/SpotifyAuth.js
+++ client/src/components/pages/Login/SpotifyAuth.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import hash from "./Hash";
 import GetTokens from "./GetTokens";
+import { authEndpoint, clientId, redirectUri, scopes } from './config';
 import localStorage from 'local-storage';
 
 export default function SpotifyAuth() {
-  const authEndpoint = "https://accounts.spotify.com/authorize"
-  const redirectUri = 'http://localhost:3000/Login';
-  const scopes = [
-      "user-top-read",
-      "user-read-currently-playing",
-      "user-read-playback-state",
-  ];
     if (!localStorage.get("userToken")) {
-      window.location.href = `${authEndpoint}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scopes.join(
+      window.location.href = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
         "%20"
       )}&response_type=token&show_dialog=true`
 
